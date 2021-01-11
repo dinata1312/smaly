326f1cefaefb22744d673e31298fba7fbfa7d52c
Christian Ardinata <42346891+dinata1312@users.noreply.github.com>
2021-01-11 20:13:16 +0700

    database

--- smaly.sql
+++ smaly.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2020 at 05:22 PM
+-- Generation Time: Jan 11, 2021 at 02:12 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -34,6 +34,14 @@ CREATE TABLE `detailpakaian` (
   `berat` int(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `detailpakaian`
+--
+
+INSERT INTO `detailpakaian` (`idDetail`, `idTransaksi`, `idPaket`, `berat`) VALUES
+(1, 1, 1, 2),
+(2, 1, 2, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -53,28 +61,8 @@ CREATE TABLE `paket` (
 --
 
 INSERT INTO `paket` (`idPaket`, `namaPaket`, `harga`, `jenis`, `durasi`) VALUES
-(1, 'Murah Njir', 5000, 'Pakaian', 3),
-(2, 'Boneka Aja', 8000, 'Boneka', 3);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `potonganharga`
---
-
-CREATE TABLE `potonganharga` (
-  `idPotongan` int(5) NOT NULL,
-  `namaPotongan` varchar(25) NOT NULL,
-  `jenisPotongan` enum('diskon','potongan harga') NOT NULL,
-  `banyakPotongan` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `potonganharga`
---
-
-INSERT INTO `potonganharga` (`idPotongan`, `namaPotongan`, `jenisPotongan`, `banyakPotongan`) VALUES
-(99862, 'GEBYAR TAHUN BARU', 'diskon', 2000);
+(1, 'Cuci Kering', 4000, 'Pakaian', 3),
+(2, 'Cuci Boneka', 8000, 'Bukan Pakaian', 3);
 
 -- --------------------------------------------------------
 
@@ -86,7 +74,6 @@ CREATE TABLE `transaksi` (
   `idTransaksi` int(5) NOT NULL,
   `namaPelanggan` varchar(15) NOT NULL,
   `status` enum('belum selesai','selesai') NOT NULL DEFAULT 'belum selesai',
-  `idPotongan` int(5) DEFAULT NULL,
   `Mulai` date DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -94,8 +81,8 @@ CREATE TABLE `transaksi` (
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`idTransaksi`, `namaPelanggan`, `status`, `idPotongan`, `Mulai`) VALUES
-(8, 'Nina', 'belum selesai', NULL, '2020-12-30');
+INSERT INTO `transaksi` (`idTransaksi`, `namaPelanggan`, `status`, `Mulai`) VALUES
+(1, 'Christian', 'selesai', '2021-01-11');
 
 -- --------------------------------------------------------
 
@@ -105,7 +92,7 @@ INSERT INTO `transaksi` (`idTransaksi`, `namaPelanggan`, `status`, `idPotongan`,
 
 CREATE TABLE `users` (
   `username` varchar(15) NOT NULL,
-  `password` varchar(255) NOT NULL,
+  `password` varchar(15) NOT NULL,
   `namaLengkap` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -135,17 +122,10 @@ ALTER TABLE `paket`
   ADD PRIMARY KEY (`idPaket`);
 
 --
--- Indexes for table `potonganharga`
---
-ALTER TABLE `potonganharga`
-  ADD PRIMARY KEY (`idPotongan`);
-
---
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`idTransaksi`),
-  ADD KEY `idPotongan` (`idPotongan`);
+  ADD PRIMARY KEY (`idTransaksi`);
 
 --
 -- Indexes for table `users`
@@ -161,25 +141,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `detailpakaian`
 --
 ALTER TABLE `detailpakaian`
-  MODIFY `idDetail` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `idDetail` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `idPaket` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `potonganharga`
---
-ALTER TABLE `potonganharga`
-  MODIFY `idPotongan` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99863;
+  MODIFY `idPaket` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `idTransaksi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `idTransaksi` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -191,12 +165,6 @@ ALTER TABLE `transaksi`
 ALTER TABLE `detailpakaian`
   ADD CONSTRAINT `detailpakaian_ibfk_1` FOREIGN KEY (`idTransaksi`) REFERENCES `transaksi` (`idTransaksi`),
   ADD CONSTRAINT `detailpakaian_ibfk_2` FOREIGN KEY (`idPaket`) REFERENCES `paket` (`idPaket`);
-
---
--- Constraints for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`idPotongan`) REFERENCES `potonganharga` (`idPotongan`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
